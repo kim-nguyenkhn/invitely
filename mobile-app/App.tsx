81579e945952b7637cadf90a4e0ab09b6c828ecc
Kim Hoang Nguyen <knguyen@lyft.com>
2020-03-27 23:00:22 -0700

    Implement basic react-navigation && EventsScreen

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,19 +1,31 @@
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+
+import { EventsScreen } from './screens/EventsScreen';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow'
+  }
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <PaperProvider theme={theme}>
+        <Stack.Navigator>
+          <Stack.Screen name="Events" component={EventsScreen} />
+        </Stack.Navigator>
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

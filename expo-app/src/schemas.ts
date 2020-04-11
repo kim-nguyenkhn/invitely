79e1a86c7299aa57a0030d6e788eabeb835dbc96
Kim Hoang Nguyen <knguyen@lyft.com>
2020-04-10 21:05:08 -0700

    feat: Change content for required fields text

--- expo-app/src/schemas.ts
+++ expo-app/src/schemas.ts
@@ -1,7 +1,10 @@
 import * as yup from 'yup';
 
 export const EventSchema = yup.object({
-    name: yup.string().max(40, 'Must be 40 characters or less').required(),
-    startTime: yup.date().required().nullable(),
+    name: yup
+        .string()
+        .max(40, 'Must be 40 characters or less')
+        .required('Please enter a name for your event.'),
+    startTime: yup.date().required('Please enter a date for your event.').nullable(),
     // TODO: implement Location
 });

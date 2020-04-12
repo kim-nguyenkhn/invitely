c3817ffb82766c1feff6b69bd60c0a9ab9f81b4f
Lena Nguyen <lena.nguyen4@gmail.com>
2020-04-12 18:23:58 +0100

    Updated init file

--- expo-app/server/__init__.py
+++ expo-app/server/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate
-from flask_restful import Api, Resource, reqparse, fields, marshal
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-api = Api(app)
+db = SQLAlchemy()
 
-from app import models
+def create_app():
+    """Construct the core application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+    db.init_app(app)
+
+    with app.app_context():
+        from . import models
+
+        # Create tables for our models
+        db.create_all()
+
+        return app

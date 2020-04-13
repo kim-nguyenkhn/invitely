b33b2b4cf4a4d83099c4517bbb93ab56f7a337ab
Kim Hoang Nguyen <knguyen@lyft.com>
2020-04-12 17:50:44 -0700

    Fix errors

--- flask-service/app/__init__.py
+++ flask-service/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from app import config
+
 db = SQLAlchemy()
 
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    app.config.from_object(config.Config)
     db.init_app(app)
 
     with app.app_context():
--- flask-service/app/models.py
+++ flask-service/app/models.py
@@ -14,26 +14,26 @@ class User(db.Model):
 # Events
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    creation_time = db.Column(db.datetime, default=datetime.utcnow, nullable=False)
+    creation_time = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     event_name = db.Column(db.String(280), nullable=False)
     event_host = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    start_date = db.Column(db.datetime, nullable=False)
-    end_date = db.Column(db.datetime, nullable=True)
-    location_address1 = db.Column(db.Str(140), nullable=False)
-    location_address2 = db.Column(db.Str(140), nullable=True)
-    location_city = db.Column(db.Str(120), nullable=True)
-    location_state = db.Column(db.Str(2), nullable=False)
-    location_zipcode = db.Column(db.Integer(5), nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=True)
+    location_address1 = db.Column(db.String(140), nullable=False)
+    location_address2 = db.Column(db.String(140), nullable=True)
+    location_city = db.Column(db.String(120), nullable=True)
+    location_state = db.Column(db.String(2), nullable=False)
+    location_zipcode = db.Column(db.String(5), nullable=False)
     host_message = db.Column(db.String(500), nullable=True)
 
 
 # guests
 class Guest(db.Model):
-    guest_id = db.Column(db.Integer, primary_key = True, Index=True, nullable=False)
+    guest_id = db.Column(db.Integer, primary_key = True, index=True, nullable=False)
     event_id = db.Column(db.Integer, db.ForeignKey('event.id'), primary_key=True, nullable=False)
-    time_added = db.Column(db.datetime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
-    telephone = db.Column(db.string(20), nullable=True)
-    email = db.Column(db.string(120), nullable=True)
+    time_added = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+    telephone = db.Column(db.String(20), nullable=True)
+    email = db.Column(db.String(120), nullable=True)
     accepted = db.Column(db.Boolean, nullable=False)
 
     # Relationships
